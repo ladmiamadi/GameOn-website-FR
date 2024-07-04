1677f5978e9026b9e1867b797eec5f1ed0bdc82d
Ladmia Madi <ladmia.madi@gmail.com>
2024-07-04 19:32:34 +0200

    add close modal functionality

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,12 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const modalClose = document.querySelector(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event
+modalClose.addEventListener("click", closeModal)
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+function closeModal() {
+   modalbg.style.display = "none";
 }
 
 
